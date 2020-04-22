9de563259706e72a4eb4ce88f1cc6d159f021d03
Monika Sahai <monikasahai03@gmail.com>
2020-04-22 01:29:04 -0400

    Added redirect to index when 404

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.get(/.*/, function (req, res) {
 	res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
 const port = process.env.PORT || 8080
 app.listen(port)
 console.log(`App is listening on port: ${port}`)
