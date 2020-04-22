4c0161599317d42a3ed09f00905c9eba5490c825
Monika Sahai <monikasahai03@gmail.com>
2020-04-22 12:15:48 -0400

    Trying more stuff

--- server.js
+++ server.js
@@ -6,18 +6,22 @@ const app = express();
 
 const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 
-app.use(staticFileMiddleware);
+// app.use(staticFileMiddleware);
 app.use(history({
-  disableDotRule: true,
-  verbose: true
+	disableDotRule: true,
+	verbose: true
 }));
 app.use(staticFileMiddleware);
 
 app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'));
+	res.render(path.join(__dirname + '/dist/index.html'));
+});
+
+app.get('/', function (req, res) {
+	res.render(path.join(__dirname + '/dist/index.html'));
 });
 
 var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
-  console.log("App now running on port", port);
+	var port = server.address().port;
+	console.log("App now running on port", port);
 });
